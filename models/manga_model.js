4ee6a6b8a11e9293b7ad23ec4d872396177ba555
Lyn Dee <lyn@Lyns-MacBook-Pro.local>
2016-07-11 17:53:42 -0400

    fixed tab

--- models/manga_model.js
+++ models/manga_model.js
@@ -3,14 +3,15 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var mangaSchema = new Schema({
-        title_japanese: String,
-        title_english: String,
-        description: String,
-        genres: [],
-        image_url_lge: String,
-        image_url_med: String,
-        publishing_status: String,
-        total_volumes: Number
+		"type": "Manga",
+        "title_japanese": String,
+        "title_english": String,
+        "description": String,
+        "genres": [],
+        "image_url_lge": String,
+        "image_url_med": String,
+        "publishing_status": String,
+        "total_volumes": Number
 });
 
 var Manga = mongoose.model('Manga', mangaSchema);
