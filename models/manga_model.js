a9a5abba4a2338190baedc79bf07cdae250f0055
Lyn Dee <lyn@Lyns-MacBook-Pro.local>
2016-07-11 18:01:01 -0400

    I changed the 'type: Manga' to 'type: String'

--- models/manga_model.js
+++ models/manga_model.js
@@ -3,14 +3,16 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var mangaSchema = new Schema({
-        title_japanese: String,
-        title_english: String,
-        description: String,
-        genres: [],
-        image_url_lge: String,
-        image_url_med: String,
-        publishing_status: String,
-        total_volumes: Number
+	var mangaSchema = new Schema({
+		"type": String,
+        "title_japanese": String,
+        "title_english": String,
+        "description": String,
+        "genres": [],
+        "image_url_lge": String,
+        "image_url_med": String,
+        "publishing_status": String,
+        "total_volumes": Number
 });
 
 var Manga = mongoose.model('Manga', mangaSchema);
