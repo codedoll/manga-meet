799ea85102e9488f9c3ef02368b72a24071953b8
Lyn Dee <lyn@Lyns-MacBook-Pro.local>
2016-07-14 12:48:14 -0400

    updated mongo

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 // require('dotenv').config();
 
-// var nani = require('nani').init("codedoll-nzqbx", "cf3rXs48cV6nUshNYrUB9zZH3u");
+var nani = require('nani').init("codedoll-nzqbx", "cf3rXs48cV6nUshNYrUB9zZH3u");
 
-Calling dependencies
+//Calling dependencies
 var express = require('express'),
     app     = express(),
     mongoose = require('mongoose'),
@@ -11,69 +11,70 @@ var express = require('express'),
    	shuffle = require('shuffle-array'),
     path = require('path');
 
-// var Manga = require('./models/manga_model.js');
+var Manga = require('./models/manga_model.js');
 
 
 
 
-// app.use(session({
-//   cookieName: 'session',
-//   secret: 'beagle',
-//   duration: 30 * 60 * 1000,
-//   activeDuration: 5 * 60 * 1000,
-//   resave: false,
-//   saveUninitialized: false
-// }));
+app.use(session({
+  cookieName: 'session',
+  secret: 'beagle',
+  duration: 30 * 60 * 1000,
+  activeDuration: 5 * 60 * 1000,
+  resave: false,
+  saveUninitialized: false
+}));
 
-// app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-// app.use(express.static('public'));
+app.use(express.static('public'));
 
-// var mangaController = require('./controllers/mangaController');
-// var userController = require('./controllers/userController');
-
-
-// app.use('/manga', mangaController);
-// app.use('/user', userController);
-
-// // SHOW ADMIN PAGE
-// app.get('/admin', function(req, res) {
-//   res.sendFile(path.resolve(__dirname + '/public/admin.html'));
-// });
-// // end admin page
-
-// // app.post('/search/', function(req, res) {
-// //   console.log(req.body.data);
-// //   console.log('at search');
-// //     nani.get('manga/search/'+req.body.data)
-// //         .then(data => {
-// //             console.log(data);
-// //             res.send(data)
-// //         })
-// //         .catch(error => {
-// //             console.log(error);
-// //         });
-// // });
+var mangaController = require('./controllers/mangaController');
+var userController = require('./controllers/userController');
 
 
+app.use('/manga', mangaController);
+app.use('/user', userController);
 
-// // MANGA GET ROUTE to INDEX.HTML
-// app.get('/manga', function(req, res) {
-// 	Manga.find(function(err, manga) {
-// 		shuffle(manga)
-// 		res.send(manga);
-// 		// console.log(manga);
-// 	});
-// });
-// // end manga route
+// SHOW ADMIN PAGE
+app.get('/admin', function(req, res) {
+  res.sendFile(path.resolve(__dirname + '/public/admin.html'));
 
+});
+// end admin page
 
-// app.get('*', function(req, res){
-//   res.redirect('/');
+// app.post('/search/', function(req, res) {
+//   console.log(req.body.data);
+//   console.log('at search');
+//     nani.get('manga/search/'+req.body.data)
+//         .then(data => {
+//             console.log(data);
+//             res.send(data)
+//         })
+//         .catch(error => {
+//             console.log(error);
+//         });
 // });
 
-// //Database name is mange_meet
+
+
+// MANGA GET ROUTE to INDEX.HTML
+app.get('/manga', function(req, res) {
+	Manga.find(function(err, manga) {
+		shuffle(manga)
+		res.send(manga);
+		// console.log(manga);
+	});
+});
+// end manga route
+
+
+app.get('*', function(req, res){
+  res.redirect('/');
+});
+
+//Database name is mange_meet
 
 var port = process.env.PORT || 3000
 var MONGODBURI = process.env.MONGODB_URI || 'mongodb://localhost/manga_meet'
